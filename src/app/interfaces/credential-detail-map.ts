c6551da3ab61849d80cf76af1d3520188bcc5940
Roger Miret <roger.miret@in2.es>
2025-09-16 16:30:36 +0200

    feat: add employeeId, mandator id, mandatee id in employee and machine detail maps

--- src/app/interfaces/credential-detail-map.ts
+++ src/app/interfaces/credential-detail-map.ts
@@ -31,15 +31,16 @@ export const CredentialDetailMap: Record<CredentialType, CredentialDetailMapEntr
       section: 'vc-fields.learCredentialEmployee.mandatee.title',
       fields: [
         { label: 'vc-fields.learCredentialEmployee.mandatee.id', valueGetter: () => s.mandate?.mandatee?.id ?? '' },
+        { label: 'vc-fields.learCredentialEmployee.mandatee.employeeId', valueGetter: () => s.mandate?.mandatee?.employeeId ?? '' },
         { label: 'vc-fields.learCredentialEmployee.mandatee.firstName', valueGetter: () => s.mandate?.mandatee?.firstName ?? '' },
         { label: 'vc-fields.learCredentialEmployee.mandatee.lastName', valueGetter: () => s.mandate?.mandatee?.lastName ?? '' },
-        { label: 'vc-fields.learCredentialEmployee.mandatee.email', valueGetter: () => s.mandate?.mandatee?.email ?? '' },
-        { label: 'vc-fields.learCredentialEmployee.mandatee.nationality', valueGetter: () => s.mandate?.mandatee?.nationality ?? '' },
+        { label: 'vc-fields.learCredentialEmployee.mandatee.email', valueGetter: () => s.mandate?.mandatee?.email ?? '' }
       ],
     },
     {
       section: 'vc-fields.learCredentialEmployee.mandator.title',
       fields: [
+        { label: 'vc-fields.learCredentialEmployee.mandator.id', valueGetter: () => s.mandate?.mandator?.id ?? '' },
         { label: 'vc-fields.learCredentialEmployee.mandator.organizationIdentifier', valueGetter: () => s.mandate?.mandator?.organizationIdentifier ?? '' },
         { label: 'vc-fields.learCredentialEmployee.mandator.organization', valueGetter: () => s.mandate?.mandator?.organization ?? '' },
         { label: 'vc-fields.learCredentialEmployee.mandator.commonName', valueGetter: () => s.mandate?.mandator?.commonName ?? '' },
@@ -61,6 +62,7 @@ export const CredentialDetailMap: Record<CredentialType, CredentialDetailMapEntr
     {
       section: 'vc-fields.lear-credential-machine.mandatee.title',
       fields: [
+        { label: 'vc-fields.lear-credential-machine.mandatee.id', valueGetter: () => s.mandate?.mandatee?.id ?? '' },
         { label: 'vc-fields.lear-credential-machine.mandatee.domain', valueGetter: () => s.mandate?.mandatee?.domain ?? '' },
         { label: 'vc-fields.lear-credential-machine.mandatee.ipAddress', valueGetter: () => s.mandate?.mandatee?.ipAddress ?? '' },
       ],
@@ -69,10 +71,11 @@ export const CredentialDetailMap: Record<CredentialType, CredentialDetailMapEntr
       section: 'vc-fields.lear-credential-machine.mandator.title',
       fields: [
         { label: 'vc-fields.lear-credential-machine.mandator.id', valueGetter: () => s.mandate?.mandator?.id ?? '' },
-        { label: 'vc-fields.lear-credential-machine.mandator.organizationIdentifier', valueGetter: () => s.mandate?.mandator?.organizationIdentifier ?? '' },
-        { label: 'vc-fields.lear-credential-machine.mandator.organization', valueGetter: () => s.mandate?.mandator?.organization ?? '' },
         { label: 'vc-fields.lear-credential-machine.mandator.commonName', valueGetter: () => s.mandate?.mandator?.commonName ?? '' },
         { label: 'vc-fields.lear-credential-machine.mandator.serialNumber', valueGetter: () => s.mandate?.mandator?.serialNumber ?? '' },
+        { label: 'vc-fields.lear-credential-machine.mandator.email', valueGetter: () => s.mandate?.mandator?.email ?? '' },
+        { label: 'vc-fields.lear-credential-machine.mandator.organization', valueGetter: () => s.mandate?.mandator?.organization ?? '' },
+        { label: 'vc-fields.lear-credential-machine.mandator.organizationIdentifier', valueGetter: () => s.mandate?.mandator?.organizationIdentifier ?? '' },
         { label: 'vc-fields.lear-credential-machine.mandator.country', valueGetter: () => s.mandate?.mandator?.country ?? '' },
       ],
     },
