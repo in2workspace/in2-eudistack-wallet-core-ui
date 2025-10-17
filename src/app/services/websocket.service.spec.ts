f0ab0f00ea8f143ef08f1d1f7f93a7e490c4e8f4
Roger Miret <roger.miret@in2.es>
2025-10-17 11:56:20 +0200

    translate PIN popup

--- src/app/services/websocket.service.spec.ts
+++ src/app/services/websocket.service.spec.ts
@@ -4,7 +4,7 @@ import { AuthenticationService } from './authentication.service';
 import { AlertController } from '@ionic/angular';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { environment } from 'src/environments/environment';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { WEBSOCKET_PATH } from '../constants/api.constants';
 import { LoaderService } from './loader.service';
 
@@ -25,6 +25,23 @@ class MockAlertController {
     });
   }
 }
+
+const translateMock = {
+  instant: jest.fn((key: string, params?: any) => {
+    switch (key) {
+      case 'confirmation.pin':
+        return 'confirmation.pin'; 
+      case 'confirmation.cancel':
+        return 'Cancel';
+      case 'confirmation.send':
+        return 'Send';
+      case 'confirmation.messageHtml':
+        return `${params?.description ?? ''}<br><small class="counter">Time remaining: ${params?.counter ?? 0} seconds</small>`;
+      default:
+        return key;
+    }
+  }),
+};
 
 describe('WebsocketService', () => {
   let mockAuthService: any;
@@ -52,7 +69,8 @@ describe('WebsocketService', () => {
         WebsocketService,
         LoaderService,
         { provide: AuthenticationService, useValue: mockAuthService },
-        { provide: AlertController, useValue: alertControllerMock }
+        { provide: AlertController, useValue: alertControllerMock },
+        { provide: TranslateService, useValue: translateMock }
       ],
     });
 
