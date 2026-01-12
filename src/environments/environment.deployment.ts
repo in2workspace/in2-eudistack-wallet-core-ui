58d527e046383672feddeb1c3cea927056fa0574
Roger Miret <roger.miret@in2.es>
2026-01-12 13:13:22 +0100

    make assets base url and favicon required 88ba0ec change naming "images" for "assets"

--- src/environments/environment.deployment.ts
+++ src/environments/environment.deployment.ts
@@ -14,12 +14,12 @@ export const environment = {
       secondary_contrast: window["env"]["secondary_contrast"] || '#000000' //OPTIONAL WITH fallback
     },
     assets: {
-      // Base URL for images (OPTIONAL with fallback)
-      base_url: window["env"]["assets_base_url"] ?? "/assets",
+      // Base URL for images (REQUIRED)
+      base_url: window["env"]["assets_base_url"],
       // Main app logo name, shown in the navbar. Points to "assets/logos/" (REQUIRED)
       logo_path: window["env"]["logo_path"],
       // App favicon. Points to "assets/icons/" (REQUIRED)
-      favicon_path: window["env"]["favicon_path"] ?? ""
+      favicon_path: window["env"]["favicon_path"]
     },
     default_lang: window["env"]["default_lang"] || "en" // OPTIONAL with fallback
   }
